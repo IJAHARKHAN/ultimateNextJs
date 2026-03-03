c3997f068e9bdaf47cae86cd827ec62589c5cbd4
IJAHARKHAN <khan7oct@gmail.com>
2026-03-04 00:31:11 +0530

    Get Latest

--- level3/src/app/layout.js
+++ level3/src/app/layout.js
@@ -1,9 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google";
+//import { Poppins } from "next/font/google";
+import { Inter } from 'next/font/google';
 import "./globals.css";
+
+// const poppins = Poppins({
+//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-poppins', // Assign a CSS variable name
+// });
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.className}`}>
       <body>
         {children}
       </body>
--- level3/src/app/page.js
+++ level3/src/app/page.js
@@ -1,4 +1,5 @@
-'use client';
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -6,16 +7,38 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
-  console.log(router);
   return (
     <>
-    <ul>
-      <Link href="/"><li>Home</li></Link>
-      <Link href="/about"><li>About</li></Link>
-      <Link href="/contact"><li>Contact</li></Link>
-    </ul>
+      <ul>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/contact">
+          <li>Contact</li>
+        </Link>
+      </ul>
 
-    <button onClick={() => router.push("/about")} role="button">Go to About</button>
+      <button onClick={() => router.push("/about")} role="button">
+        Go to About
+      </button>
+
+
+
+      <div>
+        <img src="/next.svg" alt="Vercel Logo" style={{ width: "250px" }} />{" "}
+        <br />
+        <Image src="/reactLogo.png" alt="React Logo" width={150} height={150} />
+        <br />
+        <img
+          src="/reactLogo.png"
+          alt="Vercel Logo"
+          style={{ width: "250px" }}
+        />{" "}
+        <br />
+      </div>
     </>
   );
 }
