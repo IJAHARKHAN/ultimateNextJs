7a9322e0c68e3d41a82afb64f365d0a7d05da821
IJAHARKHAN <khan7oct@gmail.com>
2026-05-17 20:41:59 +0530

    Get Latest

--- level4/src/app/learn-typescript.ts
+++ level4/src/app/learn-typescript.ts
@@ -4,7 +4,7 @@ let c:boolean = true
 let d:any = "This can be any type"
 let e: number[] = [1, 2, 3, 4, 5]
 let f: string[] = ["TypeScript", "JavaScript", "Python"]
-let g: (number | string)[] = [1, 2, 3, 4, 5, 'six', 10, 'eleven'];
+let g: (number | string | boolean)[] = [1, 2, 3, 4, 5, 'six', 10, 'eleven', false];
 
 // Function with typed parameters and return type
 function add(x: number, y: number): number {
@@ -52,3 +52,76 @@ let someValue: any = "This is a string";
 let strLength: number = (someValue as string).length;
 console.log("String Length:", strLength);
 
+
+type A = {
+    name: string;    
+}
+
+type B = {   
+    age: number;
+}
+
+type C = A & B; // Intersection type
+
+let personC: C = {
+    name: "Bob",
+    age: 25
+};
+
+console.log("Person C:", personC);
+
+
+interface D {
+    name: string;
+}
+
+interface E extends D {
+    age: number;
+}
+
+let personE: E = {
+    name: "Charlie",
+    age: 28
+};
+
+console.log("Person E:", personE);
+
+
+function greet (a: string, b: number): void {
+   console.log("Hello " + a + " " + b);
+}
+
+greet("John", 2);
+
+
+// use generics to create a function , object that can work with any type of data
+
+function identity<T>(arg: T): T {
+    return arg;
+}
+
+console.log("Identity with number:", identity<number>(42));
+console.log("Identity with string:", identity<string>("Hello TypeScript"));
+
+interface User<T>{
+    name: string,
+    age: T
+}
+
+let printUser:User<number> = {
+    name: "David",
+    age: 35
+}
+
+console.log("printUser:", printUser);
+
+
+let printMoon:Moon = {
+    name: "Luna",
+    age: 4.5
+}
+
+console.log("printMoon:", printMoon);
+
+
+// https://www.w3schools.com/js/exercise.asp?x=xrcise_object_property3
